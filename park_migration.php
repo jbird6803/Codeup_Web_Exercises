237374ca490bd65069bd2a9ab4e7f1cc04de216a
Jason <jbird6803@gmail.com>
2014-11-18 15:52:43 -0600

    first commit

---
+++ park_migration.php
@@ -0,0 +1,25 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+
+define('DB_NAME', 'national_parks_db');
+
+define('DB_USER', 'np_user');
+
+define('DB_PASS', 'password');
+
+require 'inc/db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$dbc->exec(
+	'CREATE TABLE national_parks (
+	    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	    name VARCHAR(240) NOT NULL,
+	    location VARCHAR(50) NOT NULL,
+	    date_established DATE NOT NULL,
+	    area_in_acres FLOAT NOT NULL,
+	    PRIMARY KEY (id)
+	)'
+);
+
